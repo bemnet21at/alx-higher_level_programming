0ecea88ad955d199a8f6463fa525c27a1effdcb2
bemnet21at <anjejobemnet@gmail.com>
2025-02-27 04:44:46 -0800

    indentation in 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -19,7 +19,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    """Calculates area of the square"""
 
+    """Calculates area of the square"""
     def area(self):
         return self.__size * self.__size
