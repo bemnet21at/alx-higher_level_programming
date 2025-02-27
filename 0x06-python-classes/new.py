a60350c966041a3fa9343847517a7f2aef9351ad
bemnet21at <anjejobemnet@gmail.com>
2025-02-27 05:06:11 -0800

    requirement

---
+++ 0x06-python-classes/new.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+"""Defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """sets and gets value for size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+
+        return self.__size * self.__size
